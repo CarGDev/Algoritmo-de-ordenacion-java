34dc5b35a87436f19a671e4a28fe6bf035ea7410
CarGDev <ingecarlos.gutierrez@gmail.com>
2020-08-23 21:11:08 -0500

    Finishing the order

--- src/CsvReader.java
+++ src/CsvReader.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Arrays;
 
 public class CsvReader {
 
@@ -50,12 +49,12 @@ public class CsvReader {
         }
 
         for (int j = 0; j < 20; j++) {
-            System.out.println(city[j]);
-            System.out.println(tempDensity[j]);
+            //System.out.println(city[j]);
+            //System.out.println(tempDensity[j]);
             density[j] = Integer.parseInt(tempDensity[j]);
-            System.out.println(density[j]);
+            //System.out.println(density[j]);
         }
-
+        quicksort(city, density, 0, 19);
 
     }
 
@@ -71,5 +70,64 @@ public class CsvReader {
         return result;
     }
 
+    private static void resultArray (String[] B, int[] A, int C) {
+        String result = null;
+        System.out.println("---------empieza" + C + "---------");
+        for (int k = 0; k < A.length; k++) {
+            if (k==0) {
+                result = "[ " + B[k] + ": " + Integer.toString(A[k]);
+            } else if (k == A.length - 1) {
+                result = result + " | " + B[k] + ": " + Integer.toString(A[k])  + " ]";
+            } else {
+                result = result + " | " + B[k] + ": " + Integer.toString(A[k]);
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static void quicksort(String[] city, int A[], int izq, int der) {
+
+        String pivoteCity=city[izq];
+        int pivote=A[izq]; // tomamos primer elemento como pivote
+        int i=izq;         // i realiza la búsqueda de izquierda a derecha
+        int j=der;         // j realiza la búsqueda de derecha a izquierda
+        int aux;
+        String auxCity;
+        System.out.println(A);
+
+        while(i < j){                          // mientras no se crucen las búsquedas
+            while(A[i] <= pivote && i < j) i++; // busca elemento mayor que pivote
+            while(A[j] > pivote) j--;           // busca elemento menor que pivote
+            if (i < j) {                        // si no se han cruzado
+                resultArray(city, A, 1);
+                auxCity = city[i];
+                aux= A[i];                      // los intercambia
+                city[i] = city[j];
+                A[i]=A[j];
+                city[j]=auxCity;
+                A[j]=aux;
+                resultArray(city, A, 2);
+            }
+        }
+
+        resultArray(city, A, 3);
+        city[izq]=city[j];
+        city[j]=pivoteCity;
+        A[izq]=A[j];      // se coloca el pivote en su lugar de forma que tendremos
+        A[j]=pivote;      // los menores a su izquierda y los mayores a su derecha
+
+        if(izq < j-1) {
+            resultArray(city, A, 4);
+            quicksort(city, A,izq,j-1);          // ordenamos subarray izquierdo
+        }
+        if(j+1 < der) {
+            resultArray(city, A, 5);
+            quicksort(city, A,j+1,der);          // ordenamos subarray derecho
+        }
+        resultArray(city, A, 6);
+        //for (int h = 0; h < 20; h++) {
+        //    System.out.println(city[h] + ": " + A[h]);
+        //}
+    }
 
 }
